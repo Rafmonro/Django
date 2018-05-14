6246e4846732fdcb0808902639570baffa21d613
Rafmonro <filito606@gmail.com>
2018-05-15 01:30:04 +0200

    Clase 7 Django

--- pd110/src/boletin/models.py
+++ pd110/src/boletin/models.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Ragistrado(models.Model):
+class Registrado(models.Model):
 	nombre=models.CharField(max_length=100, blank=True, null=True)
 	email=models.EmailField()
 	timestamp=models.DateTimeField(auto_now_add=True, auto_now=False)
